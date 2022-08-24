309e2c4025f0463841e44694bf3fc48ff4a6fb3a
Giovani Olaia <giovani.olaia@etec.sp.gov.br>
2022-08-24 15:08:03 -0300

    Adicionar arquivos de projeto.

---
+++ APP_Hotel/APP_Hotel/View/Hospedagem_Calculada.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace APP_Hotel.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Hospedagem_Calculada : ContentPage
+    {
+        public Hospedagem_Calculada()
+        {
+            InitializeComponent();
+        }
+    }
+}
